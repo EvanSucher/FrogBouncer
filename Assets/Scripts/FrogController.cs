696e9bcac722cb86db352380f3279cd62c0d7cac
Evan Sucher <evan.sucher@loop.colum.edu>
2018-11-14 13:37:31 -0600

    Added Comments
    Added lots of comments, tweeked a few variables with the player

--- Assets/Scripts/FrogController.cs
+++ Assets/Scripts/FrogController.cs
@@ -35,21 +35,20 @@ public class FrogController : MonoBehaviour {
     CircleCollider2D frogCollider;
 
 
-
     private float closeCursorDistance = 0.3f;
-    bool isCharging = false;
-    bool isLaunching = false;
-    int launchTime = 0;
-    int chargeTime = 0;
-    int timeBeforeLaunchDecceleration = 140;
-    float deccel = 0.98f;
-    float stopSpeed = 5;
-    float RotationSpeed = 4;
-    float gravIncreaseRate = 0.01f;
+    private bool isCharging = false;
+    private bool isLaunching = false;
+    private int launchTime = 0;
+    private int chargeTime = 0;
+    private int timeBeforeLaunchDecceleration = 200;
+    private float deccel = 0.99f;
+    private float stopSpeed = 5;
+    private float RotationSpeed = 4;
+    private float gravIncreaseRate = 0.01f;
 
     // Use this for initialization
     void Start ()
-    {
+    {   // Initialize the animator, circle collider and circle collider material
         anim = GetComponent<Animator>();
         frogCollider = GetComponent<CircleCollider2D>();
         frogCollider.sharedMaterial = stopMaterial;
@@ -60,14 +59,16 @@ public class FrogController : MonoBehaviour {
     {
         float move = Input.GetAxis("Horizontal");
 
-        CheckChargeInput();
-        if (!isLaunching)
-        {
+        CheckChargeInput(); // Check's if the player is charging in order to update isCharging and isLaunching
 
-            if (!isCharging) //not charging
+        if (!isLaunching) // Not Launching
+        {
+            if (!isCharging) // Not Charging or Launching, basically when the player is idle
             {
+                // This line is let's the player walk left and right while not charging or launching
                 GetComponent<Rigidbody2D>().velocity = new Vector2(move * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
-                if (move > 0 && !facingRight)
+
+                if (move > 0 && !facingRight) // Flips the player depending on direction
                 {
                     Flip();
                 }
@@ -78,7 +79,7 @@ public class FrogController : MonoBehaviour {
             }
             else // is charging
             {
-                if (fCrosshair.isPointingRight() && !facingRight)
+                if (fCrosshair.isPointingRight() && !facingRight) // Flips Crosshair based on mouse location (SHOULD BE CHANGED DEPENDING ON ANGLE)
                 {
                     Flip();
                 }
@@ -90,14 +91,14 @@ public class FrogController : MonoBehaviour {
         }
         else
         {
-            if (launchTime >= timeBeforeLaunchDecceleration) // THis conditional should be replaced with whenever it hits the floor
+            if (launchTime >= timeBeforeLaunchDecceleration) // After the player has been launching for a given time
             {
-                GetComponent<Rigidbody2D>().gravityScale = 1;
+                GetComponent<Rigidbody2D>().gravityScale = 1; //sets gravity to 1 (should already be 1 by this point but just in case)
 
                 Vector2 tempVelocityVector = new Vector2(GetComponent<Rigidbody2D>().velocity.x * deccel, GetComponent<Rigidbody2D>().velocity.y * deccel);
-                GetComponent<Rigidbody2D>().velocity = tempVelocityVector;
+                GetComponent<Rigidbody2D>().velocity = tempVelocityVector; // Slowly deccelerates the player
             }
-            else
+            else // When the player is launching but before a set time, the gravity slowly increments to get greater over time
             {
                 if(GetComponent<Rigidbody2D>().gravityScale < 1)
                 {
@@ -118,33 +119,37 @@ public class FrogController : MonoBehaviour {
 
     public void CheckChargeInput()
     {
-        if(!isLaunching)
+        if(!isLaunching) //If the  player is Launching, then don't do anything
         {
             if (!isCharging) // Not Charging
             {
-                if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0)) // Pressing Space while not charging makes you charge
+                if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0)) // Pressing Space/mouse1 while not charging makes you charge
                 {
                     isCharging = true;
                 }
             }
             else // charging
             {
-                if (!Input.GetKey("space") && !Input.GetMouseButton(0))  // Letting go of space while charging makes you stop charging
+                if (!Input.GetKey("space") && !Input.GetMouseButton(0))  // Letting go of space/mouse1 while charging makes you stop charging
                 {
-                    float charge = (10 + chargeTime) / 60f;
-                    // ================EXPERIMENTAL CODE: DANGER DANGER==============================
-                    //GetComponent<Rigidbody2D>().velocity = fCrosshair.GetNormalizedAimVector()*launchSpeed;
+                    float charge = (10 + chargeTime) / 60f; // charge is the value that 
+
+                    // ================ EXPERIMENTAL CODE: DANGER DANGER (not really) ======================
+                    // This basically just changes the velocity to the normal vector of the crosshair * the launchspeed * charge
                     GetComponent<Rigidbody2D>().velocity = new Vector2(fCrosshair.GetNormalizedAimVector().x * (launchSpeed*charge), fCrosshair.GetNormalizedAimVector().y * (launchSpeed * charge));
-                    //=================================Nice==========================================
-                    isCharging = false;
+                    // =================================Nice================================================
+
+                    isCharging = false; // Going from charging to launching
                     isLaunching = true;
                     frogCollider.sharedMaterial = launchMaterial;
                     fCrosshair.transform.localScale = new Vector3(2, 2, fCrosshair.transform.localScale.z);
 
+                    RotationSpeed = 10;
+
                     GetComponent<Rigidbody2D>().gravityScale = 0.5f + (charge/120);
                     chargeTime = 0;
                 }
-                if (Input.GetMouseButton(1))
+                if (Input.GetMouseButton(1))  // Pressing the right mouse button will stop the charging and make the player go back to idle
                 {
                     isCharging = false;
                     chargeTime = 0;
@@ -152,10 +157,10 @@ public class FrogController : MonoBehaviour {
                     transform.rotation = Quaternion.Euler(0, 0, 0);
                 }
 
-                if (chargeTime < 50)
+                if (chargeTime < 50) // Increases charge time while the player is charging, and increase UI SIZE with charge
                 {
                     chargeTime++;
-                    float tempScale = 1 + (chargeTime / 50f) * 2;
+                    float tempScale = 1 + (chargeTime / 50f) * 2; // Scale for the Crosshairs
                     fCrosshair.transform.localScale = new Vector3(tempScale, tempScale, fCrosshair.transform.localScale.z);
                     mCrosshair.transform.localScale = new Vector3(1+tempScale, 1+tempScale, fCrosshair.transform.localScale.z);
                 }
@@ -164,22 +169,26 @@ public class FrogController : MonoBehaviour {
 
     }
 
-    void OnCollisionEnter2D(Collision2D other)
-    {
+    void OnCollisionEnter2D(Collision2D other) // function called when there's a collision with the wall
+    {   
         float currentVelocity = Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) + Mathf.Abs(GetComponent<Rigidbody2D>().velocity.y) * 0.6f;
-        RotationSpeed = currentVelocity - 3;
-        Debug.Log(currentVelocity);
-        if (isLaunching)
+        RotationSpeed = currentVelocity - 3; // Thsese lines take the velocity of the player, and set the player rotation speed to match velocity
+
+        if (isLaunching) // None of the code in here will run unless the player is launching
         {
-            if (Input.GetKey("space") || Input.GetMouseButton(0))
+            if (Input.GetKey("space") || Input.GetMouseButton(0)) // If the player is holding either space or 
             {
+                // The if statements below, are basically designed to get the contact points of whatever the player collides with
+                // and then finds the normal vector and compares it to Vector2.Up, Vector2.Left, etc. So far there is only up, down,
+                // left, and right but the code could probably just be changed so there are no if statements, and there is just code
+                // that turns the normal vector into an Euler angle, and set that as the transform.rotation
                 if (other.contacts[0].normal == Vector2.up) //ground
                 {
                     Debug.Log("ground");
-                    StopLaunch();
-                    transform.rotation = Quaternion.Euler(0, 0, 0);
-                    GetComponent<Rigidbody2D>().gravityScale = 0;
-                    isCharging = true;
+                    StopLaunch(); // Stops the Launch
+                    transform.rotation = Quaternion.Euler(0, 0, 0); // Set the Rotation
+                    GetComponent<Rigidbody2D>().gravityScale = 0; //Change Gravity to 0
+                    isCharging = true; //Immediately begin charging again
 
                 }
                 else if (other.contacts[0].normal == Vector2.left) //right wall
@@ -207,13 +216,13 @@ public class FrogController : MonoBehaviour {
                     isCharging = true;
                 }
             }
-            if (other.contacts[0].normal == Vector2.up && currentVelocity < stopSpeed) //if you hit the ground after a certain amount of time
+            if (other.contacts[0].normal == Vector2.up && currentVelocity < stopSpeed) //if the player is slow enough and hits the ground
             {
                 Debug.Log("ground");
-                StopLaunch();
+                StopLaunch(); // returns the player to idle
                 transform.rotation = Quaternion.Euler(0, 0, 0);
             }
-            if (other.contacts[0].normal == Vector2.right || other.contacts[0].normal == Vector2.left)
+            if (other.contacts[0].normal == Vector2.right || other.contacts[0].normal == Vector2.left) // Only flips the player if hits a wall
             {
                 Flip();
             }
@@ -221,7 +230,7 @@ public class FrogController : MonoBehaviour {
 
     }
 
-    void Flip()
+    void Flip() //just flips the player
     {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
@@ -229,23 +238,23 @@ public class FrogController : MonoBehaviour {
         transform.localScale = theScale;
     }
 
-    void StopLaunch()
+    void StopLaunch() // function to stop the player from launching
     {
         isLaunching = false;
-        launchTime = 0;
-        frogCollider.sharedMaterial = stopMaterial;
-        GetComponent<Rigidbody2D>().gravityScale = 1;
-        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        launchTime = 0; // reset launch timer
+        frogCollider.sharedMaterial = stopMaterial; // changes material so the player does not bounce
+        GetComponent<Rigidbody2D>().gravityScale = 1; // sets gravity to normal
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0); // sets velocity to normal
     }
 
     void RotateInAir()
     {
-        float tempRotationZ = -RotationSpeed;
-        if (facingRight)
+        float tempRotationZ = -RotationSpeed; // temporary rotation float 
+        if (facingRight) 
         {
-            tempRotationZ = RotationSpeed;
+            tempRotationZ = RotationSpeed; // temporary rotation float is opposite if facing right
         }
-        Vector3 tempRotationVector = new Vector3(0, 0, tempRotationZ);
-        transform.Rotate(tempRotationVector);
+        Vector3 tempRotationVector = new Vector3(0, 0, tempRotationZ); // vector3 to prime rotation
+        transform.Rotate(tempRotationVector); // Rotates the player
     }
 }
